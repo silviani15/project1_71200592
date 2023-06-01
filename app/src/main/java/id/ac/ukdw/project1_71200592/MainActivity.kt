4623e3d4d88ce4285e0f70f2fa295d39f391b468
silviani15 <silviani.vanesha@ti.ukdw.ac.id>
2023-06-01 09:28:35 +0700

    project1

---
+++ app/src/main/java/id/ac/ukdw/project1_71200592/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.ac.ukdw.project1_71200592
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
